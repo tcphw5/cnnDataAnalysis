b23eb06af78f7b09f5a3efb7a77de18bff9d807a
tcphw5 <tcphw5@mst.edu>
2017-02-09 13:46:39 -0600

    analysis file on home pc

---
+++ CNN articles/analysisCNN.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+analysis of CNN articles
+
+Created on Wed Feb  8 11:23:03 2017
+
+@author: tpwin10
+"""
+
+import numpy as np
+
+tokenizedOutput = "tokenizedOutput1_1.txt"
+
+#data = np.loadtxt(tokenizedOutput, dtype=np.dtype(str))
+data = np.genfromtxt(tokenizedOutput, dtype=str)
+
+for word in np.nditer(data, op_flags=['readwrite']):
+    if word == "," or word == "." or word == ")":
+        #print("hi")
+        word[...] = word[0:-1]
+        print(word)
+
+
+
+unique, counts = np.unique(data, return_counts=True)
+
+wordcounts = np.asarray((unique, counts)).T
+
+sortedwordcounts = wordcounts[np.argsort(wordcounts[:, 1])[::-1]]
+
+fileOUT = open('Output.txt', 'w', encoding='utf-8')
+
+
+for word in sortedwordcounts:
+    fileOUT.write(word[0] + " " + word[1] + "\n")
